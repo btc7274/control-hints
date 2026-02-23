67d5895f88b5ecc8b6ab13a58e8c364e6784dd8c
btc7274 <downs_dude09@icloud.com>
2026-02-23 14:05:51 -0500

    Update ControlHints.lua
    Removed unused services, generalized iasSetup parameter in CreateUI to accept any Instance

--- module/ControlHints.lua
+++ module/ControlHints.lua
@@ -1,28 +1,23 @@
 --!strict
-local UserInputService = game:GetService("UserInputService")
-local ContentProvider = game:GetService("ContentProvider")
-local CollectionService = game:GetService("CollectionService")
 
 -- Classes
 local Hint = require(script.Core.Hint)
 
 -- ModuleScripts
 local Settings = require(script.Configuration.Settings)
-local Sorting = require(script.Configuration.KeyCodeSorting)
 local Utilities = require(script.Core.Utilities)
 
 local ControlHints = {}
 
 
--- Creates a ScreenGui that updates with the given Folder
--- iasSetup: A Folder containing all of your InputContexts, InputActions, and InputBindings (nested appropriately)
+-- Creates a ScreenGui that updates with the given IAS setup
+-- iasSetup: An Instance containing InputContexts, InputActions, and InputBindings (nested in that order)
 -- optionalParent: Sets the parent of the ScreenGui
-function ControlHints.CreateUI(iasSetup: Folder, optionalParent: Instance?): ScreenGui
+function ControlHints.CreateUI(iasSetup: Instance, optionalParent: Instance?): ScreenGui
 
 	-- Error handling for iasSetup
-	assert(iasSetup, "Folder 'iasSetup' is missing!")
-	assert(typeof(iasSetup) == "Instance", "Folder 'iasSetup' is not an Instance. but is a " .. typeof(iasSetup) .. "!")
-	assert(iasSetup:IsA("Folder"), "Folder 'iasSetup' is not a Folder, but is a " .. iasSetup.ClassName .. "!")
+	assert(iasSetup, "Instance 'iasSetup' is missing!")
+	assert(typeof(iasSetup) == "Instance", "'iasSetup' is not an Instance, but is a " .. typeof(iasSetup) .. "!")
 
 	-- Error handling for optionalParent
 	if optionalParent then
@@ -41,31 +36,31 @@ function ControlHints.CreateUI(iasSetup: Folder, optionalParent: Instance?): Scr
 	local templateHint = container.TemplateHint
 	assert(templateHint, "Frame 'TemplateHint' not found in Frame 'Container'!")
 	templateHint.Visible = false
-	
+
 	-- Set parent (if given)
 	if optionalParent then
 		newScreenGui.Parent = optionalParent
 	end
-	
+
 	-- Get setup info for hint creation
 	local setupInfo: Utilities.SetupInfo = {
 		screenWidth = newScreenGui.AbsoluteSize.X,
 		inputType = Utilities.GetInputType()
 	}
-	
+
 	-- Track InputActions that have hints (avoid duplication)
 	local assignedHints: {[InputAction]: boolean} = {}
-	
+
 	-- Helper function
 	local function createHint(inputAction: InputAction, inputContext: InputContext)
 
 		if assignedHints[inputAction] then return end
 		assignedHints[inputAction] = true
-		
+
 		Hint.new(inputAction, inputContext, newScreenGui, setupInfo)
-		
+
 	end
-	
+
 	-- Listen for new descendants of the iasSetup Folder
 	local descAddedConn: RBXScriptConnection
 	descAddedConn = iasSetup.DescendantAdded:Connect(function(desc)
@@ -92,13 +87,13 @@ function ControlHints.CreateUI(iasSetup: Folder, optionalParent: Instance?): Scr
 			task.defer(createHint, inputAction, inputContext)
 		end
 	end
-	
+
 	-- Disconnect all connections, clear memory (hints are auto-destroyed)
 	local function cleanUp()
 		descAddedConn:Disconnect()
 		table.clear(assignedHints)
 	end
-	
+
 	-- Clean up when ScreenGui is destroying
 	newScreenGui.Destroying:Connect(cleanUp)
 
