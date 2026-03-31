33f023c16a7a97da366967d1f886dca2057383a5
btc7274 <downs_dude09@icloud.com>
2026-03-31 15:38:14 -0400

    Update ControlHints.lua

--- module/ControlHints.lua
+++ module/ControlHints.lua
@@ -9,6 +9,19 @@ local Utilities = require(script.Core.Utilities)
 
 local ControlHints = {}
 
+local connections: {RBXScriptConnection} = {}
+
+
+-- Disconnect all connections
+local function clearConnections()
+
+	for _, connection in connections do
+		connection:Disconnect()
+	end
+	table.clear(connections)
+
+end
+
 
 -- Creates a ScreenGui that updates with the given IAS setup
 -- iasSetup: An Instance containing InputContexts, InputActions, and InputBindings (nested in that order)
@@ -24,14 +37,16 @@ function ControlHints.CreateUI(iasSetup: Instance, optionalParent: Instance?): S
 		assert(typeof(optionalParent) == "Instance", "Instance 'optionalParent' is not an Instance, but is a " .. typeof(optionalParent) .. "!")
 	end
 
+	clearConnections()
+
 	-- Cache display style
 	local displayStyle = Settings.DISPLAY_STYLE
 
 	-- Create new ScreenGui from display style
-	local newScreenGui = displayStyle:Clone()
+	local controlHintsGui = displayStyle:Clone()
 
 	-- Error handling, hide template hint
-	local container = newScreenGui.Container
+	local container = controlHintsGui.Container
 	assert(container, "Frame 'Container' not found in display style '" .. displayStyle.Name .. "'!")
 	local templateHint = container.TemplateHint
 	assert(templateHint, "Frame 'TemplateHint' not found in Frame 'Container'!")
@@ -39,45 +54,53 @@ function ControlHints.CreateUI(iasSetup: Instance, optionalParent: Instance?): S
 
 	-- Set parent (if given)
 	if optionalParent then
-		newScreenGui.Parent = optionalParent
+		controlHintsGui.Parent = optionalParent
 	end
 
 	-- Get setup info for hint creation
 	local setupInfo: Utilities.SetupInfo = {
-		screenWidth = newScreenGui.AbsoluteSize.X,
+		screenWidth = controlHintsGui.AbsoluteSize.X,
 		inputType = Utilities.GetInputType()
 	}
 
 	-- Track InputActions that have hints (avoid duplication)
 	local assignedHints: {[InputAction]: boolean} = {}
 
-	-- Helper function
+	-- Create a unique Hint object
 	local function createHint(inputAction: InputAction, inputContext: InputContext)
 
+		-- Pass if not assigned to hint
 		if assignedHints[inputAction] then return end
 		assignedHints[inputAction] = true
 
-		Hint.new(inputAction, inputContext, newScreenGui, setupInfo)
+		Hint.new(inputAction, inputContext, controlHintsGui, setupInfo)
 
 	end
 
-	-- Listen for new descendants of the iasSetup Folder
-	local descAddedConn: RBXScriptConnection
-	descAddedConn = iasSetup.DescendantAdded:Connect(function(desc)
-		if desc:IsA("InputContext") then
-			local inputContext = desc
+	-- Create a hint when a descendant is added to iasSetup
+	table.insert(connections, iasSetup.DescendantAdded:Connect(function(descendant)
+		if descendant:IsA("InputContext") then
+			local inputContext = descendant
 			for _, inputAction in inputContext:GetChildren() do
 				if not inputAction:IsA("InputAction") then continue end
 				createHint(inputAction, inputContext)
 			end
-		elseif desc:IsA("InputAction") then
-			local inputAction = desc
+		elseif descendant:IsA("InputAction") then
+			local inputAction = descendant
 			local inputContext = inputAction:FindFirstAncestorOfClass("InputContext")
 			if inputContext then
 				createHint(inputAction, inputContext)
 			end
+		elseif descendant:IsA("InputBinding") then
+			local inputAction = descendant:FindFirstAncestorOfClass("InputAction")
+			if inputAction then
+				local inputContext = inputAction:FindFirstAncestorOfClass("InputContext")
+				if inputContext then
+					createHint(inputAction, inputContext)
+				end
+			end
 		end
-	end)
+	end))
 
 	-- Populate UI with hints
 	for _, inputContext in iasSetup:GetChildren() do
@@ -88,16 +111,11 @@ function ControlHints.CreateUI(iasSetup: Instance, optionalParent: Instance?): S
 		end
 	end
 
-	-- Disconnect all connections, clear memory (hints are auto-destroyed)
-	local function cleanUp()
-		descAddedConn:Disconnect()
-		table.clear(assignedHints)
-	end
+	-- Clean up when ScreenGui or iasSetup is destroying
+	table.insert(connections, controlHintsGui.Destroying:Once(clearConnections))
+	table.insert(connections, iasSetup.Destroying:Once(clearConnections))
 
-	-- Clean up when ScreenGui is destroying
-	newScreenGui.Destroying:Connect(cleanUp)
-
-	return newScreenGui
+	return controlHintsGui
 
 end
 
